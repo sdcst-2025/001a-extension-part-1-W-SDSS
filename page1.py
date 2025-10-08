ffe35b7c2e984e7c180444407aeb4a39b0511906
W-SDSS <wakanai027@deltalearns.ca>
2025-10-08 10:28:55 -0700

    w

--- page1.py
+++ page1.py
@@ -1,6 +1,13 @@
-print("Welcome to your psychological test!!")
-print("Today you will know your character.")
+print("Welcome to the Forest of onfusion!!")
+print("Your lost in the forest.")
 print("")
-print("What are you doing now?")
-print("1. You found a creature.")
-print("2. You are camping.")
+print("Which way to go?")
+print("1. Go right into the dark road.")
+print("2. Go left into the bright road. ")  
+
+choice = input("Choose 1 or 2")
+
+if choice == "1" :
+ import page2a
+if choice == "2": 
+ import page2b
---
+++ page2b.py
@@ -0,0 +1,8 @@
+print("Do you walk carefully down the dark road, or do you run because its too scary?")
+
+choice = input("Choose 1 if you walk carefully. Choose 2 if you run.")
+
+if choice == "1":
+    import page2b3
+if choice == "2":
+    import page2b4
