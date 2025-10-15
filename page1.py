9c7b01891a5051001bb804b1dcc9c0b47a7cc878
W-SDSS <wakanai027@deltalearns.ca>
2025-10-15 10:35:28 -0700

    w

--- page1.py
+++ page1.py
@@ -1,10 +1,10 @@
-print("Welcome to the Forest of onfusion!!")
-print("Your lost in the forest.")
+print("Welcome to the Forest of Confusion!!")
+print("You are lost in the forest.")
 print("")
 print("Which way to go?")
-print("1. Go right into the dark road.")
-print("2. Go left into the bright road. ")  
-
+print("1. Go right into the dark way.")
+print("2. Go left into the bright way. ")  
+print("")
 choice = input("Choose 1 or 2")
 
 if choice == "1" :
---
+++ page2a.py
@@ -0,0 +1,13 @@
+print("It is so dark that you can not see anything.")
+print("Do you want to walk carefully?")
+print("Or do you want to run because you are scary?")
+print("")
+choice = input("Choose 1 or 2")
+print("1.walk carefully")
+print("2.run")
+print("")
+
+if choice == "1" :
+ import page2a3a
+if choice == "2": 
+ import page2b3b
